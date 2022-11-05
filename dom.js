81dbda7ad8fa5a54f5636d9075bdb8459776fa07
DaTT2001 <congdatylvt@gmail.com>
2022-11-05 07:59:34 +0700

    on off windows

---
+++ dom.js
@@ -0,0 +1,9 @@
+const a = document.querySelector(".chat-box")
+const b = document.querySelector(".icon")
+function off() {
+    a.classList.toggle("chat-box1")
+}
+
+b.onclick = function () {
+    console.log('ấdsdass')
+}
